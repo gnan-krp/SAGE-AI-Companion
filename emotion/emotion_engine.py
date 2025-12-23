61452fc09a0532ba814077c02a9c39dbb66c224f
Gnan Parekh <gnan0839@gmail.com>
2025-12-23 14:14:08 +0530

    Update emotion_engine.py

--- emotion/emotion_engine.py
+++ emotion/emotion_engine.py
@@ -1,1 +1,18 @@
 
+class EmotionEngine:
+    def __init__(self):
+        self.emotion = "idle"
+
+    def update_emotion(self, event):
+        if event == "voice_detected":
+            self.emotion = "happy"
+        elif event == "no_interaction":
+            self.emotion = "bored"
+        else:
+            self.emotion = "idle"
+
+        return self.emotion
+
+    def get_emotion(self):
+        return self.emotion
+
